899a3299e2b6504fb9279ee263255777edc89877
ajang369 <jhjun99421@gmail.com>
2025-02-02 15:27:23 +0900

    setting/#11 : 디렉토리 구조 설정

---
+++ Android/app/src/main/java/com/example/firstproject/data/repository/MainRepository.kt
@@ -0,0 +1,4 @@
+package com.example.firstproject.data.repository
+
+object MainRepository {
+}
---
+++ Android/app/src/main/java/com/example/firstproject/ui/common/CommonUI.kt
@@ -0,0 +1,8 @@
+package com.example.firstproject.ui.common
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CommonUI() {
+
+}
