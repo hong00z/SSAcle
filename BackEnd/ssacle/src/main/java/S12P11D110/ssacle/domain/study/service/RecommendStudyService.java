11e5de9425f8a56ad6b95072900f247d44da1eab
Lee Mungyeong <moon943380@gmail.com>
2025-02-06 13:11:50 +0900

    feat/#33: 매칭로직-코사인유사도 수정

--- BackEnd/ssacle/src/main/java/S12P11D110/ssacle/domain/study/service/RecommendStudyService.java
+++ BackEnd/ssacle/src/main/java/S12P11D110/ssacle/domain/study/service/RecommendStudyService.java
@@ -5,19 +5,45 @@ import S12P11D110.ssacle.domain.study.dto.StudyDTO;
 import S12P11D110.ssacle.domain.study.dto.UserConditionDTO;
 import org.springframework.stereotype.Service;
 
+import java.awt.*;
 import java.util.*;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class RecommendStudyService {
     public List<RecommendStudyDTO> recommendStudy(UserConditionDTO userCondition, List<StudyDTO>allStudiesDTO){
+        Set<String> userTopics = userCondition.getTopics().stream()
+                .map(Enum::name)
+                .collect(Collectors.toSet());
+
         //1. 스터디 필터링
         List<StudyDTO> filteredStudies = allStudiesDTO.stream()
                 .filter(study -> study.getCount()  > study.getMembers().size() ) // 정원 > 가입멤버수인 스터디
-                .filter(study -> !study.getMembers().contains(userCondition.getUserId()))// 유저가 가입된 스터디는 제외
+                .filter(study -> study.getTopic().stream().map(Enum::name).anyMatch(userTopics::contains)) //anyMatch(Predicate<T>) → 스트림에서 하나라도 조건을 만족하면 true 반환.
+                .filter(study -> { // member, wishMember preMember는 제외
+                    // member, wishMember preMember객체 가져오기
+                    Set<String> members = study.getMembers() != null ? study.getMembers() : new HashSet<>();
+                    Set<String> wishMembers = study.getWishMembers() != null ? study.getWishMembers() : new HashSet<>();
+                    Set<String> preMembers = study.getPreMembers() != null ? study.getPreMembers() : new HashSet<>();
+
+                    boolean isAlreadyMembers = members.contains(userCondition.getUserId().toString());
+                    boolean isAlreadyWishMembers = wishMembers.contains(userCondition.getUserId().toString());
+                    boolean isAlreadyPreMembers = preMembers.contains(userCondition.getUserId().toString());
+
+
+                    System.out.println("스터디ID"+ study.getStudyId()+", 이미 가입한 스터디인가?"+ isAlreadyMembers);
+                    System.out.println("스터디ID"+ study.getStudyId()+", 이미 요청받은 스터디인가?"+ isAlreadyWishMembers);
+                    System.out.println("스터디ID"+ study.getStudyId()+", 이미 가입 신청한 스터디인가?"+ isAlreadyPreMembers);
+
+                    return !(isAlreadyMembers||isAlreadyWishMembers||isAlreadyPreMembers);
+                })
                 .collect(Collectors.toList());
 
+        System.out.println(filteredStudies);
+
         // 2. 코사인 유사도를 기반으로 스터디 추천
+
         Map<StudyDTO, Double> studySimilarityMap = new HashMap<>();
         for(StudyDTO study : filteredStudies){
             double similarity = calculateCosineSimilarity(userCondition, study);
