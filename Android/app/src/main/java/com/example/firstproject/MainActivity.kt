52155e7814e6e089da6931c406fcbbf79129665a
ajang369 <jhjun99421@gmail.com>
2025-02-08 03:01:48 +0900

    feat/#41 : 스터디 등록화면 UI 구현
    텍스트필드, 주제 선택 카드뷰, 요일 선택 아이템, NumberPicker, 화면 이동 구현

--- Android/app/src/main/java/com/example/firstproject/MainActivity.kt
+++ Android/app/src/main/java/com/example/firstproject/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.firstproject.databinding.ActivityMainBinding
@@ -14,6 +15,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,8 +25,8 @@ class MainActivity : AppCompatActivity() {
 
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.fragment_container) as NavHostFragment
+        navController = navHostFragment.navController
 
-        val navController = navHostFragment.navController
         binding.bottomNavigationView.setupWithNavController(navController)
 
     }
