f9cd0000eeddd33f3b185e92bef911408d086e6a
ajang369 <jhjun99421@gmail.com>
2025-02-11 19:17:33 +0900

    feat/#54 : Retrofit 기본설정 및 JWT 토큰 저장, 카카오 로그인 통신 기능 구현

--- Android/app/src/main/java/com/example/firstproject/ui/mypage/MypageFragment.kt
+++ Android/app/src/main/java/com/example/firstproject/ui/mypage/MypageFragment.kt
@@ -1,18 +1,28 @@
 package com.example.firstproject.ui.mypage
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.example.firstproject.data.repository.MainRepository
 import com.example.firstproject.databinding.FragmentMypageBinding
+import com.example.firstproject.ui.chat.TAG
+import timber.log.Timber
+import com.kakao.sdk.user.UserApiClient
+import kotlinx.coroutines.launch
 
 class MypageFragment : Fragment() {
     private var _binding : FragmentMypageBinding? = null
     private val binding get() = _binding!!
 
+    val repository = MainRepository
 
-    override fun onCreateView(
+        override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,6 +30,27 @@ class MypageFragment : Fragment() {
         _binding = FragmentMypageBinding.inflate(inflater, container, false)
 
         binding.apply {
+            loginButton.setOnClickListener {
+
+
+
+                val context = requireContext()
+                UserApiClient.instance.loginWithKakaoTalk(context) { token, error ->
+                    val accessToken = {token?.accessToken}
+                    Timber.d("사용할 토큰: $accessToken")
+                    if(error != null) {
+
+                    } else if (token != null) {
+                        Timber.tag(TAG).e("로그인 성공 %s",token.accessToken)
+                        // 통신
+                        viewLifecycleOwner.lifecycleScope.launch {
+                            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                                repository.loginWithKakao(token.accessToken)
+                            }
+                        }
+                    }
+                }
+            }
 
         }
         return binding.root
