1b641ef07b59f9fa57e1389b788c30d5f5ed5489
이문경 <moon943380@gmail.com>
2025-02-13 10:01:09 +0900

    feat/#68: study restApi 수정 2차

--- BackEnd/ssacle/src/main/java/S12P11D110/ssacle/domain/study/dto/Study.java
+++ BackEnd/ssacle/src/main/java/S12P11D110/ssacle/domain/study/dto/Study.java
@@ -1,24 +1,22 @@
 package S12P11D110.ssacle.domain.study.dto;
 
-import lombok.AllArgsConstructor;
+
 import lombok.Builder;
 import lombok.Data;
 
 import java.util.Set;
 
-
-// RecommendStudyService에 쓰이는 DTO
 @Data
 @Builder
-@AllArgsConstructor
-public class RecoStudyResult {
+//스터디 정보를 담은 DTO
+public class Study {
     private String studyId;
-    private double similarity;
     private String studyName;
     private String topic;
     private Set<String> meetingDays;
     private int count;
     private Set<String> members;
-    private  String createdBy;
-
+    private Set<String> wishMembers;
+    private Set<String> preMembers;
+    private String createdBy;
 }
--- BackEnd/ssacle/src/main/java/S12P11D110/ssacle/domain/study/dto/nicknameImage.java
+++ BackEnd/ssacle/src/main/java/S12P11D110/ssacle/domain/study/dto/nicknameImage.java
@@ -8,7 +8,7 @@ import lombok.Data;
 @Data
 @Builder
 @AllArgsConstructor
-// 내가 참여중인 스터디 - 멤버 - 멤버 닉네임/ 이미지
+// 내가 참여중인 스터디 - 멤버 - 멤버 닉네임/ 이미지/방장여부
 public class nicknameImage {
     private String nickname;
     private String image;
