35b40ab0e1476ede26b6f656a164d59114ea4cac
이문경 <moon943380@gmail.com>
2025-02-15 16:07:20 +0900

    fix/#93: 폴더 찾는 함수 추가

--- BackEnd/ssacle/src/main/java/S12P11D110/ssacle/domain/study/service/StudyService.java
+++ BackEnd/ssacle/src/main/java/S12P11D110/ssacle/domain/study/service/StudyService.java
@@ -20,6 +20,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.File;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -716,6 +717,29 @@ public class StudyService {
 
     }
     // GPT: to
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
