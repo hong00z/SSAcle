ebdc9d3e31007312df3ecec83f1a167782224a3c
ajang369 <jhjun99421@gmail.com>
2025-02-15 22:29:37 +0900

    feat/#89 : 로그인 화면, 사용자 인증 화면 UI 구현

--- Android/app/src/main/java/com/example/firstproject/ui/LoginAuth/LoginScreen.kt
+++ Android/app/src/main/java/com/example/firstproject/ui/LoginAuth/LoginScreen.kt
@@ -3,13 +3,18 @@ package com.example.firstproject.ui.LoginAuth
 import android.util.Log
 import android.widget.ImageButton
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,8 +22,11 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -40,22 +48,45 @@ fun LoginScreen(
     LaunchedEffect(loginState) {
 
         if (loginState is RequestResult.Success) {
-            Log.d("로그인 화면","onSuccess: ${loginState}")
+            Log.d("로그인 스크린: ", "onSuccess: ${loginState}")
             navController.navigate("Auth")
         }
     }
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.White)
     ) {
-        Button(onClick = {
-            viewModel.loginWithKakao(context)
 
-        }, modifier = Modifier.fillMaxWidth().height(60.dp)) {
-            Text("카카오 로그인")
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.ssacle_logo), null,
+                    modifier = Modifier
+                        .fillMaxWidth(0.6f)
+                        .height(110.dp)
+                )
+
+
+            }
         }
 
+        Box(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 100.dp)
+        ) {
+            Image(
+                painter = painterResource(R.drawable.kakao_login_button), null,
+                modifier = Modifier
+                    .clickable { viewModel.loginWithKakao(context) }
+            )
+        }
     }
 }
