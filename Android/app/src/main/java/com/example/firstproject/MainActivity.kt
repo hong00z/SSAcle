2b5d110ef164afcc1274953e07f84bb5328a3a16
ajang369 <jhjun99421@gmail.com>
2025-02-16 16:17:36 +0900

    feat/#94 : 로그인, 인증, 온보딩 완료 상태마다 LoginActivity에서 화면 시작 위치 설정

--- Android/app/src/main/java/com/example/firstproject/MainActivity.kt
+++ Android/app/src/main/java/com/example/firstproject/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.firstproject.data.repository.TokenManager
 import com.example.firstproject.databinding.ActivityMainBinding
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,6 +26,23 @@ class MainActivity : AppCompatActivity() {
 
         if (accessToken.isNullOrEmpty()) {
             Log.d("메인 액티비티", "로그인 만료. 로그인 화면으로 이동")
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+            return
+        }
+
+        // 2. DataStore에서 사용자의 '인증 완료 여부', '온보딩 완료 여부' 확인
+        //   - isAuthCompleted(), isOnboardingCompleted()는 MyApplication.kt 등에서 정의해두었다 가정
+        val isAuthCompleted = runBlocking {
+            MyApplication.isAuthCompleted().first()  // Boolean
+        }
+        val isOnboardingCompleted = runBlocking {
+            MyApplication.isOnboardingCompleted().first()  // Boolean
+        }
+
+        // 3. 인증/온보딩 미완료라면 -> LoginActivity로 다시 보냄
+        if (!isAuthCompleted || !isOnboardingCompleted) {
+            Log.d("메인 액티비티", "인증 혹은 온보딩 미완료 상태. 로그인 액티비티로 이동")
             startActivity(Intent(this, LoginActivity::class.java))
             finish()
             return
