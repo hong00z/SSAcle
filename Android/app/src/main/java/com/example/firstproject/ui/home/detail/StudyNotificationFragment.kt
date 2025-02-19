0410c07861c6c2a6bbf5d0a53fbe7c8006484acf
ajang369 <jhjun99421@gmail.com>
2025-02-19 14:14:06 +0900

    feat/#136 : 스터디 수신함에서 가입 신청 조회 및 수락 기능 구현

---
+++ Android/app/src/main/java/com/example/firstproject/ui/home/detail/StudyNotificationFragment.kt
@@ -0,0 +1,58 @@
+package com.example.firstproject.ui.home.detail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.firstproject.databinding.FragmentStudyNotificationBinding
+import com.example.firstproject.ui.common.CommonTopBar
+import com.google.android.material.tabs.TabLayoutMediator
+
+
+class StudyNotificationFragment : Fragment() {
+    private var _binding: FragmentStudyNotificationBinding? = null
+    private val binding get() = _binding!!
+
+    lateinit var studyId: String
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentStudyNotificationBinding.inflate(inflater, container, false)
+        studyId = arguments?.getString("studyId")!!
+
+
+        binding.apply {
+            studyTopbarComposeView.setContent {
+
+                CommonTopBar(
+                    title = "스터디 수신함",
+                    onBackPress = {
+                        requireActivity().onBackPressedDispatcher.onBackPressed()
+                    }
+                )
+            }
+
+            studyViewPager.adapter = DetailNotificationPagerAdapter(requireActivity(), studyId)
+
+            TabLayoutMediator(studyTabLayout, studyViewPager) { tab, position ->
+                tab.text = if (position == 0) "초대 현황" else "가입 요청"
+
+            }.attach()
+        }
+
+
+
+        return binding.root
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
