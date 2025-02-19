7416265551636cd4e54fe1e5267d33ebae05aa03
ajang369 <jhjun99421@gmail.com>
2025-02-19 15:41:18 +0900

    feat/#137 : 사용자의 알람 수신함 통신 구현

--- Android/app/src/main/java/com/example/firstproject/ui/home/notification/NotificationFragment.kt
+++ Android/app/src/main/java/com/example/firstproject/ui/home/notification/NotificationFragment.kt
@@ -2,62 +2,140 @@ package com.example.firstproject.ui.home.notification
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.viewpager2.widget.ViewPager2
 import com.example.firstproject.R
+import com.example.firstproject.data.model.dto.response.MyAppliedStudyListDtoItem
+import com.example.firstproject.data.model.dto.response.MyInvitedStudyListDtoItem
+import com.example.firstproject.data.repository.MainRepository
+import com.example.firstproject.databinding.FragmentNotificationBinding
 import com.example.firstproject.ui.common.CommonTopBar
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
+import com.rootachieve.requestresult.RequestResult
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class NotificationFragment : Fragment() {
+    private var _binding: FragmentNotificationBinding? = null
+    private val binding get() = _binding!!
 
-    private lateinit var tabLayout: TabLayout
-    private lateinit var viewPager: ViewPager2
-    private lateinit var backButton: ImageButton
-    private lateinit var pagerAdapter: NotificationPagerAdapter
-    private lateinit var topBarComposeView: ComposeView
+    val repository = MainRepository
 
+    // ✅ 리스트를 StateFlow로 관리하여 값 변경 감지 가능
+    private val _inviteList = MutableStateFlow<List<MyAppliedStudyListDtoItem>>(emptyList())
+    val inviteList: StateFlow<List<MyAppliedStudyListDtoItem>> get() = _inviteList
 
-    @SuppressLint("MissingInflatedId")
+    private val _studyList = MutableStateFlow<List<MyInvitedStudyListDtoItem>>(emptyList())
+    val studyList: StateFlow<List<MyInvitedStudyListDtoItem>> get() = _studyList
+
+    private val notificationViewModel: NotificationViewModel by viewModels()
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = FragmentNotificationBinding.inflate(inflater, container, false)
 
-        val view = inflater.inflate(R.layout.fragment_notification, container, false)
+        observeViewModel()
+        notificationViewModel.getMyAppliedInfo()
+        notificationViewModel.getInviteStudyInfo()
 
-        tabLayout = view.findViewById<TabLayout>(R.id.tabLayout)
-        viewPager = view.findViewById(R.id.viewPager)
-//        backButton = view.findViewById(R.id.back_button)
-        topBarComposeView = view.findViewById(R.id.topbar_compose_view)
-
-        topBarComposeView.setContent {
-
-            CommonTopBar(
-                title = "나의 알림함",
-                onBackPress = {
-                    requireActivity().onBackPressedDispatcher.onBackPressed()
-                }
-            )
+        // ✅ StateFlow를 사용하여 값 변경을 자동 감지
+        viewLifecycleOwner.lifecycleScope.launch {
+            inviteList.collect { list ->
+                Log.d("내 수신함 통신완료", "신청 현황: ${list}") // ✅ 값 변경 시 자동 출력
+            }
         }
 
-//        backButton.setOnClickListener {
-//            requireActivity().onBackPressedDispatcher.onBackPressed()
-//        }
-        pagerAdapter = NotificationPagerAdapter(requireActivity())
-        viewPager.adapter = pagerAdapter
+        viewLifecycleOwner.lifecycleScope.launch {
+            studyList.collect { list ->
+                Log.d("내 수신함 통신완료", "초대온거: ${list}") // ✅ 값 변경 시 자동 출력
+            }
+        }
 
-        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
-            tab.text = if (position == 0) "내 신청 현황" else "내 수신함"
-        }.attach()
+        binding.apply {
+            topbarComposeView.setContent {
+
+                CommonTopBar(
+                    title = "나의 알림함",
+                    onBackPress = {
+                        requireActivity().onBackPressedDispatcher.onBackPressed()
+                    }
+                )
+            }
+
+            viewPager.adapter = NotificationPagerAdapter(requireActivity(), inviteList, studyList)
+
+            TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+                tab.text = if (position == 0) "내 신청 현황" else "내 수신함"
+            }.attach()
+        }
+
+        return binding.root
+    }
+
+    // viewModel 통신
+    private fun observeViewModel() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                notificationViewModel.myAppliedResult.collect { result ->
+                    when (result) {
+                        is RequestResult.Success -> {
+                            _inviteList.value = result.data
+
+                        }
+
+                        is RequestResult.Failure -> {
+                            Log.e("Mypage", "오류 발생: ${result.exception?.message}")
+                        }
+
+                        else -> Unit
+                    }
+                }
+
+            }
+        }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                notificationViewModel.inviteResult.collect { result ->
+                    when (result) {
+                        is RequestResult.Success -> {
+                            _studyList.value = result.data
+
+                        }
+
+                        is RequestResult.Failure -> {
+                            Log.e("Mypage", "오류 발생: ${result.exception?.message}")
+                        }
+
+                        else -> Unit
+                    }
+                }
 
 
-        return view
+            }
+        }
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- Android/app/src/main/java/com/example/firstproject/ui/home/notification/NotificationPagerAdapter.kt
+++ Android/app/src/main/java/com/example/firstproject/ui/home/notification/NotificationPagerAdapter.kt
@@ -1,18 +1,37 @@
 package com.example.firstproject.ui.home.notification
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.firstproject.data.model.dto.response.MyAppliedStudyListDtoItem
+import com.example.firstproject.data.model.dto.response.MyInvitedStudyListDtoItem
+import kotlinx.coroutines.flow.StateFlow
 
-class NotificationPagerAdapter(fragmentActivity: FragmentActivity) :
+class NotificationPagerAdapter(
+    fragmentActivity: FragmentActivity,
+    private val inviteList: StateFlow<List<MyAppliedStudyListDtoItem>>,
+    private val studyList: StateFlow<List<MyInvitedStudyListDtoItem>>
+) :
     FragmentStateAdapter(fragmentActivity) {
 
     override fun getItemCount(): Int = 2 // 2개의 페이지 (내 신청 현황, 내 수신함)
 
     override fun createFragment(position: Int): Fragment {
         return when (position) {
-            0 -> RequestListFragment() // 내 신청 현황
-            1 -> InboxListFragment()   // 내 수신함
+            // 내 신청 현황
+            0 -> RequestListFragment().apply {
+                arguments = Bundle().apply {
+                    
+                }
+            }
+            // 내 수신함
+            1 -> InboxListFragment().apply {
+                arguments = Bundle().apply {
+
+                }
+            }
+
             else -> throw IllegalStateException("Unexpected position $position")
         }
     }
